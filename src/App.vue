67225ca5dbf806a7666ce9adaa9e131c0db2a3b8
Jo Dunham <scaffelpike@yahoo.com.au>
2021-06-03 18:23:08 +1000

    set up of project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id='app'>
+    @{{ user.username }}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  date() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'CodeTravelled',
+        firstName: 'Jo',
+        lastname: 'Dunham',
+        email: 'joDunham@email.com',
+        isAdmin: true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
