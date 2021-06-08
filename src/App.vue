643ad5946da2d79206c3102ff35b0861297b9700
Jo Dunham <scaffelpike@yahoo.com.au>
2021-06-08 16:45:13 +1000

    clean up and separate userprofile

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,25 @@
 <template>
-  <div id='app'>
-    @{{ user.username }} - {{ fullName }}
-    <strong>Followers: </strong>{{ followers }}
-    <!-- <button v-on:click="followUser">Follow</button> these two lines are the same!! --> 
-    <button @click="followUser">Follow</button>
-  </div>
+    <div id='app'>
+        <UserProfile/>
+    </div>
 </template>
 
 <script>
+import UserProfile from "./components/UserProfile";
 
 export default {
   name: 'App',
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'CodeTravelled',
-        firstName: 'Jo',
-        lastName: 'Dunham',
-        email: 'joDunham@email.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch: { //watches something and runs every time it changes
-    followers(newFollowerCount, oldFollowerCount) { //give it the same name as what you are watching just to make it readable
-      if(oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`);
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    }
-  },
-  
-  mounted() { //run every time component is loaded for the first time, or remounted, each time starts the first time. Lifecycle hook
-    this.followUser();
-  }
+  components: { UserProfile }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    min-height: 100vh;
+    background-color: #F3F5FA;
+  }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class='user-profile'>
+    <div class='user-profile__user-panel'>
+      <h1 class='user-profile__username'>@{{ user.username }}</h1> 
+        <div class='user-profile__follower-count'>
+          <strong>Followers: </strong>{{ followers }}
+    <!-- <button v-on:click="followUser">Follow</button> 
+    <button @click="followUser">Follow</button>  these two lines are the same!! --> 
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'CodeTravelled',
+        firstName: 'Jo',
+        lastName: 'Dunham',
+        email: 'joDunham@email.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: { //watches something and runs every time it changes
+    followers(newFollowerCount, oldFollowerCount) { //give it the same name as what you are watching just to make it readable
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  
+  mounted() { //run every time component is loaded for the first time, or remounted, each time starts the first time. Lifecycle hook
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+    .user-profile {
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        width: 100%;
+        padding: 50px 5%;
+    }
+
+    .user-profile__user-panel{
+        display: flex;
+        flex-direction: column;
+        margin-right: 50px;
+        padding: 20px;
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #DFE3E8;
+    }
+
+    h1 {
+        margin: 0;
+    }
+</style>
