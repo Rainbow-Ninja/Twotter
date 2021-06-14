4f9e8843253a0631d9a44a5ada01f5587f2807c7
Jo Dunham <scaffelpike@yahoo.com.au>
2021-06-14 17:06:42 +1000

    twoot type added

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -9,9 +9,20 @@
     <!-- <button v-on:click="followUser">Follow</button> 
     <button @click="followUser">Follow</button>  these two lines are the same!! --> 
         </div>
-        <form class="user-profile__create-twoot">
+        <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
             <label for="newTwoot"><strong>New Twoot</strong></label>
-            <textarea id="newTwoot" rows="4"/>
+            <textarea id="newTwoot" rows="4" v-model="newTwootContent" />
+            <div class="user-profile__create-twoot-type">
+                <label for="newTwootType"><strong>Type: </strong></label>
+                <select id="newTwootType" v-model="selectedTwootType">
+                    <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+                        {{ option.name }}
+                    </option>
+                </select>
+            </div>
+            <button>
+                Twoot!
+            </button>
         </form>
       </div>
       <div class="user-profile__twoots-wrapper">
@@ -34,6 +45,12 @@ export default {
   components: { TwootItem },
   data() {
     return {
+      newTwootContent: '',
+      selectedTwootType: 'instant',
+      twootTypes: [
+        {value: 'draft', name: 'Draft'},
+        {value: 'instant', name: 'Instant Twoot'}
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -69,6 +86,15 @@ export default {
     },
     toggleFav(id) {
         console.log(`Favourited Twoot #${id}`);
+    },
+    createNewTwoot() {
+        if(this.newTwootContent && this.selectedTwootType !== 'draft') {
+            this.user.twoots.unshift({
+                id: this.user.twoots.length + 1,
+                content: this.newTwootContent
+            })
+            this.newTwootContent='';
+        }
     }
   },
   
